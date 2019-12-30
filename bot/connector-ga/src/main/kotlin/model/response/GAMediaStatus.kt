4ebd1c999d6928f2f88048886433790e3a1f761a
Vermue Nicolas <nicolasvermue@gmail.com>
2019-12-30 10:41:07 +0100

    google media response

---
+++ bot/connector-ga/src/main/kotlin/model/response/GAMediaStatus.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2017/2019 e-voyageurs technologies
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ai.tock.bot.connector.ga.model.response
+
+import ai.tock.bot.connector.ga.model.GAIntent
+import ai.tock.bot.connector.ga.model.request.GAArgumentValue
+import ai.tock.bot.connector.ga.model.request.GAArgumentValueType
+
+/**
+ * @see https://developers.google.com/actions/reference/rest/Shared.Types/MediaStatus
+ */
+enum class GAMediaStatus {
+    STATUS_UNSPECIFIED,
+    FINISHED, // Media has stopped because playback finished.
+    FAILED // Failed to play media
+}
+
+/*data class GAMediaStatusValueSpec(
+    val status: GAMediaStatus = GAMediaStatus.STATUS_UNSPECIFIED
+): GAInputValueData(GAIntent.mediaStatus.type!!)*/
+
+data class GAMediaStatusValue(
+    val status: GAMediaStatus
+) : GAArgumentValue(
+    GAArgumentValueType.mediaStatus
+)
